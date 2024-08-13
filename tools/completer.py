a65ec0441ce9bd6ae8f3167d063ce6e7f82f3c55
Kostiantyn Grebets <kog@yousee.dk>
2024-08-13 22:02:28 +0300

    add command completion

---
+++ tools/completer.py
@@ -0,0 +1,17 @@
+from prompt_toolkit.completion import Completer, Completion
+
+class CommandCompleter(Completer):
+
+    def __init__(self, commands):
+        super().__init__()
+        self.commands = commands
+
+    def get_completions(self, document, complete_event):
+        text_before_cursor = document.text_before_cursor
+       
+        if ' ' in text_before_cursor:
+            return
+        
+        for cmd in self.commands:
+            if cmd.startswith(text_before_cursor):
+                yield Completion(cmd, start_position=-len(text_before_cursor))
